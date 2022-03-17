78198a21aaa58ddbd69747775ce52d89636403d6
Yusuke Nojima <nojima@ynojima.com>
2022-03-18 00:07:24 +0900

    Introduce clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("grpcall")
+        .arg(Arg::new("addr").index(1).required(true))
+        .arg(Arg::new("method").index(2).required(true))
+        .get_matches();
+
+    println!("addr   = {:?}", matches.value_of("addr").unwrap());
+    println!("method = {:?}", matches.value_of("method").unwrap());
 }
