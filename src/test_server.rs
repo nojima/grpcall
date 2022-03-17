4f9d819a11059c3637ad42bebd67dfcc61c4c85d
Yusuke Nojima <nojima@ynojima.com>
2022-03-18 00:34:54 +0900

    Add gRPC test server

---
+++ src/test_server.rs
@@ -0,0 +1,42 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use helloworld::greeter_server::{Greeter, GreeterServer};
+use helloworld::{HelloReply, HelloRequest};
+
+pub mod helloworld {
+    tonic::include_proto!("helloworld");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        eprintln!("Got a request: {:?}", request);
+
+        let reply = helloworld::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    eprintln!("Server start: addr={addr}");
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
